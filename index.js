b0d4f1f2f0dcb4b6c9ab17e40a41737e59530a27
vinittiwari90 <47031784+vinittiwari90@users.noreply.github.com>
2020-02-03 13:59:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()
 //Define request response in root URL (/) and response with text Hello World!
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('From Git Hello World!')
 })
 //Launch listening server on port 8080 and consoles the log.
 app.listen(8080, function () {
